662005dc88a6f0acf16d9e59a6cc9816058f1623
Juan Adonis <adn221294@gmail.com>
2025-08-01 14:23:12 -0400

    Rest API tutorail completed

---
+++ Rest-API-Totorial/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
+
+// album represents data about a record album.
+type album struct {
+    ID     string  `json:"id"`//Estas etiquetas determinan la clave en el JSON.
+    Title  string  `json:"title"`
+    Artist string  `json:"artist"`
+    Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+//Esta es la "Base de Datos" del tutoral.
+var albums = []album{
+    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func main() {
+    router := gin.Default()
+    router.GET("/albums", getAlbums)
+    router.POST("/albums", postAlbum)
+	router.GET("/albums/:id", getAlbumByID)
+	router.Run("localhost:8080")
+}
+
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")//Para obtener url params.
+
+	for _, al := range albums {
+		if al.ID == id {
+			c.IndentedJSON(http.StatusFound, al)
+			return
+		}
+	}
+	
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found :( "})
+}
+
+//Como en JS, Go hace hoisting.
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbum(c *gin.Context) {
+
+	var newAlbum album
+
+	err := c.BindJSON(&newAlbum)//Esta funcion toma el puntero a newAlbum y 'vuelca' la info del JSON en la struct 
+	if err != nil {
+		fmt.Println(err)//Pendiente ver los errores que da gin.
+		return
+	}
+
+	albums = append(albums, newAlbum)
+    //Envia la response:
+	c.IndentedJSON(http.StatusCreated, newAlbum.ID)
+}
