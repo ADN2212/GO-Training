9c7775bfe00dc38dd1802d6f8c1c0ac5850b01bd
Juan Adonis Nunez Collado <adn221294@gmail.com>
2025-08-31 16:15:45 -0400

    sectors and cities gruped by parent id

--- JSON-processing/main.go
+++ JSON-processing/main.go
@@ -19,9 +19,9 @@ type City struct {
 }
 
 type Sector struct {
-	CityId     uint   `json:"municipio_id"`
-	SectorId uint `json:"sector_id"`
-	Name string `json:"sector"`
+	CityId   uint   `json:"municipio_id"`
+	SectorId uint   `json:"sector_id"`
+	Name     string `json:"sector"`
 }
 
 func main() {
@@ -55,10 +55,10 @@ func main() {
 		return
 	}
 
-	fmt.Println("Lista de provincias:----------------------------------------------------------------------")
-	for i := range provs {
-		fmt.Println(provs[i].Name)
-	}
+	// fmt.Println("Lista de provincias:----------------------------------------------------------------------")
+	// for i := range provs {
+	// 	fmt.Println(provs[i].Name)
+	// }
 
 	//TODO: abstraer toda esta logica repetida usando un funcion que retorne un generic type.
 	cityFile, cityErr := os.Open("./data/municipios.json")
@@ -89,14 +89,88 @@ func main() {
 		return
 	}
 
-	fmt.Println("Lista de cuidades: ----------------------------------------------------------------------")
-	for i := range cities {
-		fmt.Println(cities[i].Name)
+	// fmt.Println("Lista de cuidades: ----------------------------------------------------------------------")
+	// for i := range cities {
+	// 	fmt.Println(cities[i].Name)
+	// }
+
+	sectorFile, sectorErr := os.Open("./data/sectores.json")
+
+	if sectorErr != nil {
+		fmt.Printf("An error ocurred while loading the provs file: %s", sectorErr.Error())
+		return
 	}
 
+	defer (func() {
+		fmt.Println("Clossing the sectors file")
+		sectorFile.Close()
+	})()
+
+	sectorBytes, sectorsReadError := io.ReadAll(sectorFile)
+
+	if sectorsReadError != nil {
+		fmt.Printf("An error ocurred while reading the sectors bytes: %s", sectorsReadError.Error())
+		return
+	}
+
+	var sectors []Sector
+
+	sectorsParseError := json.Unmarshal(sectorBytes, &sectors)
+
+	if sectorsParseError != nil {
+		fmt.Printf("An error ocurred while trying to parse the sectors: %s", sectorsParseError.Error())
+		return
+	}
+
+	// fmt.Println("Lista de sectores: ----------------------------------------------------------------------")
+	// for i := range sectors {
+	// 	fmt.Println(sectors[i])
+	// }
+
+	//Este map servira para agrupar los sectores por cuidad
+	sectorsMap := make(map[uint][]Sector) //De esta forma se declara e inizializa el map al mismo tiempo.
+	var currentSector Sector
+
+	for i := range sectors {
+		currentSector = sectors[i]
+		if len(sectorsMap[currentSector.CityId]) == 0 {
+			sectorsMap[currentSector.CityId] = []Sector{currentSector}
+		} else {
+			sectorsMap[currentSector.CityId] = append(sectorsMap[currentSector.CityId], currentSector)
+		}
+	}
+
+	//fmt.Println(sectorsMap)
+	// for cityId, sectorsArray := range sectorsMap {
+	// 	fmt.Println(cityId)
+	// 	for i := range sectorsArray {
+	// 		fmt.Println(sectorsArray[i])
+	// 	}
+	// }
+
+	//El siguiente para aculumar las cuidades por porvincia:
+	citiesMap := make(map[uint][]City)
+	var currentCity City
+
+	for i := range cities {
+
+		currentCity = cities[i]
+
+		if len(citiesMap[currentCity.ProvinceId]) == 0 {
+			citiesMap[currentCity.ProvinceId] = []City{currentCity}
+		} else {
+			citiesMap[currentCity.ProvinceId] = append(citiesMap[currentCity.ProvinceId], currentCity)
+		}
+	}
+
+	// for provId, cityArray := range citiesMap {
+	// 	fmt.Println(provId)
+	// 	for i := range cityArray {
+	// 		fmt.Println(cityArray[i])
+	// 	}
+	// }
+
 	
-
-
 
 
 
