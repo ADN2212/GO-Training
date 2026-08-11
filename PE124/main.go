d6c99915ef265e4a33aa8d311321cb88c03c7ec1
Juan Adonis <adn221294@gmail.com>
2026-08-11 11:29:17 -0400

    linear solution

---
+++ PE124/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"cmp"
+	"fmt"
+	"slices"
+	"time"
+)
+
+type pair struct {
+	n      uint64
+	radVal uint64
+}
+
+func main() {
+	start := time.Now()
+	n := uint64(1)
+	rads := []pair{}
+
+	limit := uint64(100_000)
+
+	for n <= limit {
+		rads = append(rads, pair{
+			n:      n,
+			radVal: rad(n),
+		})
+		n += 1
+	}
+
+	slices.SortFunc(rads, func(p1, p2 pair) int {
+		if p1.radVal == p2.radVal {
+			return cmp.Compare(p1.n, p2.n)
+		}
+		return cmp.Compare(p1.radVal, p2.radVal)
+	})
+
+	k := 10_000
+	fmt.Println(rads[k-1].n)
+	fmt.Printf("Completado en %v segundos\n", time.Since(start).Seconds())
+}
+
+// O(n)
+func rad(n uint64) uint64 {
+	factors := []uint64{}
+
+	for n%2 == 0 {
+		if !slices.Contains(factors, 2) {
+			factors = append(factors, 2)
+		}
+		n /= 2
+	}
+
+	d := uint64(3)
+	for d*d <= n {
+		for n%d == 0 {
+			if !slices.Contains(factors, d) {
+				factors = append(factors, d)
+			}
+			n /= d
+		}
+		d += 2
+	}
+
+	if n > 1 {
+		if !slices.Contains(factors, d) {
+			factors = append(factors, n)
+		}
+	}
+
+	product := uint64(1)
+	for _, f := range factors {
+		product *= f
+	}
+
+	return product
+}
